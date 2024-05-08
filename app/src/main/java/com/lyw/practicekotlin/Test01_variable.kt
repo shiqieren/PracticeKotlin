732a2b2b764f18a9042303a8092d18dbad4e7f20
liyiwei <liyiwei@honghe-tech.com>
2024-05-08 11:04:02 +0800

    创建第一个kt file and main 函数

--- app/src/main/java/com/lyw/practicekotlin/Test01_variable.kt
+++ app/src/main/java/com/lyw/practicekotlin/Test01_variable.kt
@@ -1,5 +1,17 @@
 package com.lyw.practicekotlin
 
+
+var a : Int = 1  //变量
+val b : Double = 2.0  //只读变量
+const val PI = 3.1415 //常量
 fun main() {
+    a= 2
+//    b = 3.0 val只能初始化一次，只读
+    val c:Float = 4.0f
     println("first test")
+    println("first test $a")
+    println("first test "+ b)
+    println("first test $c")
+    println("first test "+ PI)
+//    const val PI = 3.1415  无非定义局部的常量
 }
